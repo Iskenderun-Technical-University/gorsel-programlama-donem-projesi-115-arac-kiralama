a2a5f939f3ede2864af683efd2b04e7419cf096d
buse kunu <108237867+busekunu@users.noreply.github.com>
2022-12-03 16:21:42 +0300

    Add files via upload

---
+++ arac-kiralama-projesi/arac-kiralama-projesi/AnaSayfa.cs
@@ -0,0 +1,15 @@
+namespace arac_kiralama_projesi
+{
+    public partial class AnaSayfa : Form
+    {
+        public AnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
