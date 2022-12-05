7640e91c28eab5f59f0fa31e4b25cac93632f3b2
YoncaEsendemir <115565688+YoncaEsendemir@users.noreply.github.com>
2022-12-05 16:29:55 +0300

    Add files via upload
    Müşteri listele

---
+++ müşteri-listeleme1/arac kiralama/frmMusteriListele.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace arac_kiralama
+{
+    partial class frmMusteriListele
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonIptal = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxTel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAdres = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxDogum = new System.Windows.Forms.TextBox();
+            this.labelSoyad = new System.Windows.Forms.Label();
+            this.textBoxSoyad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxTc = new System.Windows.Forms.TextBox();
+            this.labelmail = new System.Windows.Forms.Label();
+            this.textBoxMail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAd = new System.Windows.Forms.TextBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.labelKm = new System.Windows.Forms.Label();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Lavender;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(505, 72);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(415, 469);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // buttonIptal
+            // 
+            this.buttonIptal.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.buttonIptal.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonIptal.ImageIndex = 0;
+            this.buttonIptal.Location = new System.Drawing.Point(186, 479);
+            this.buttonIptal.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.buttonIptal.Name = "buttonIptal";
+            this.buttonIptal.Size = new System.Drawing.Size(114, 63);
+            this.buttonIptal.TabIndex = 35;
+            this.buttonIptal.Text = "Iptal";
+            this.buttonIptal.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonIptal.UseVisualStyleBackColor = true;
+            this.buttonIptal.Click += new System.EventHandler(this.buttonIptal_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(56, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 25);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "Tel : ";
+            // 
+            // textBoxTel
+            // 
+            this.textBoxTel.Location = new System.Drawing.Point(186, 288);
+            this.textBoxTel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxTel.Name = "textBoxTel";
+            this.textBoxTel.Size = new System.Drawing.Size(247, 27);
+            this.textBoxTel.TabIndex = 32;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(56, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 25);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Adres :";
+            // 
+            // textBoxAdres
+            // 
+            this.textBoxAdres.Location = new System.Drawing.Point(186, 239);
+            this.textBoxAdres.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxAdres.Name = "textBoxAdres";
+            this.textBoxAdres.Size = new System.Drawing.Size(247, 27);
+            this.textBoxAdres.TabIndex = 30;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(56, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 25);
+            this.label5.TabIndex = 29;
+            this.label5.Text = "Doğum Tarih :";
+            // 
+            // textBoxDogum
+            // 
+            this.textBoxDogum.Location = new System.Drawing.Point(186, 140);
+            this.textBoxDogum.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxDogum.Name = "textBoxDogum";
+            this.textBoxDogum.Size = new System.Drawing.Size(247, 27);
+            this.textBoxDogum.TabIndex = 28;
+            // 
+            // labelSoyad
+            // 
+            this.labelSoyad.AutoSize = true;
+            this.labelSoyad.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelSoyad.Location = new System.Drawing.Point(56, 95);
+            this.labelSoyad.Name = "labelSoyad";
+            this.labelSoyad.Size = new System.Drawing.Size(74, 25);
+            this.labelSoyad.TabIndex = 27;
+            this.labelSoyad.Text = "Soyad :";
+            // 
+            // textBoxSoyad
+            // 
+            this.textBoxSoyad.Location = new System.Drawing.Point(186, 91);
+            this.textBoxSoyad.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxSoyad.Name = "textBoxSoyad";
+            this.textBoxSoyad.Size = new System.Drawing.Size(247, 27);
+            this.textBoxSoyad.TabIndex = 26;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(56, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 25);
+            this.label3.TabIndex = 25;
+            this.label3.Text = " TC No :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxTc
+            // 
+            this.textBoxTc.Location = new System.Drawing.Point(186, 189);
+            this.textBoxTc.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxTc.Name = "textBoxTc";
+            this.textBoxTc.Size = new System.Drawing.Size(247, 27);
+            this.textBoxTc.TabIndex = 24;
+            // 
+            // labelmail
+            // 
+            this.labelmail.AutoSize = true;
+            this.labelmail.BackColor = System.Drawing.Color.Transparent;
+            this.labelmail.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelmail.Location = new System.Drawing.Point(56, 341);
+            this.labelmail.Name = "labelmail";
+            this.labelmail.Size = new System.Drawing.Size(86, 25);
+            this.labelmail.TabIndex = 23;
+            this.labelmail.Text = "e-posta :";
+            // 
+            // textBoxMail
+            // 
+            this.textBoxMail.Location = new System.Drawing.Point(186, 337);
+            this.textBoxMail.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxMail.Name = "textBoxMail";
+            this.textBoxMail.Size = new System.Drawing.Size(247, 27);
+            this.textBoxMail.TabIndex = 22;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(56, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Adi :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxAd
+            // 
+            this.textBoxAd.Location = new System.Drawing.Point(186, 41);
+            this.textBoxAd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxAd.Name = "textBoxAd";
+            this.textBoxAd.Size = new System.Drawing.Size(247, 27);
+            this.textBoxAd.TabIndex = 20;
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnGuncelle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuncelle.ImageIndex = 1;
+            this.btnGuncelle.Location = new System.Drawing.Point(33, 479);
+            this.btnGuncelle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(130, 63);
+            this.btnGuncelle.TabIndex = 19;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // labelKm
+            // 
+            this.labelKm.AutoSize = true;
+            this.labelKm.Font = new System.Drawing.Font("Segoe UI", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelKm.Location = new System.Drawing.Point(240, -27);
+            this.labelKm.Name = "labelKm";
+            this.labelKm.Size = new System.Drawing.Size(145, 28);
+            this.labelKm.TabIndex = 18;
+            this.labelKm.Text = "Kimlik Bilgiler";
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnSil.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSil.ImageIndex = 0;
+            this.btnSil.Location = new System.Drawing.Point(333, 479);
+            this.btnSil.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(102, 63);
+            this.btnSil.TabIndex = 35;
+            this.btnSil.Text = "Sil";
+            this.btnSil.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(634, 16);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 27);
+            this.textBox1.TabIndex = 24;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(504, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 25);
+            this.label2.TabIndex = 25;
+            this.label2.Text = " TC Ara";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // frmMusteriListele
+            // 
+            this.AccessibleName = " ";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(967, 607);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.buttonIptal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxTel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxAdres);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDogum);
+            this.Controls.Add(this.labelSoyad);
+            this.Controls.Add(this.textBoxSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxTc);
+            this.Controls.Add(this.labelmail);
+            this.Controls.Add(this.textBoxMail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxAd);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.labelKm);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMusteriListele";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Musteri Listeleme Sayfasi";
+            this.Load += new System.EventHandler(this.frmMusteriListele_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button buttonIptal;
+        private Label label6;
+        private TextBox textBoxTel;
+        private Label label4;
+        private TextBox textBoxAdres;
+        private Label label5;
+        private TextBox textBoxDogum;
+        private Label labelSoyad;
+        private TextBox textBoxSoyad;
+        private Label label3;
+        private TextBox textBoxTc;
+        private Label labelmail;
+        private TextBox textBoxMail;
+        private Label label1;
+        private TextBox textBoxAd;
+        private Button btnGuncelle;
+        private Label labelKm;
+        private Button btnSil;
+        private TextBox textBox1;
+        private Label label2;
+    }
+}
